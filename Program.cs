63deb31805471c2025dc859a95aa0d58213e8351
alexrocha97 <venturarocha08@gmail.com>
2022-09-13 20:29:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá! Qual é o seu nome? ");
+var nome = Console.ReadLine();
+Console.WriteLine("Olá," + nome);
+
+Console.WriteLine("Hello, World!");
